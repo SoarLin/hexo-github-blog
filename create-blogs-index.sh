4d424f62c1aa0976dbfc0df2b359cbb6bfb3c724
Soar <soar@stco.tw>
2016-11-12 17:48:32 +0800

    建立index傳入mapping黨內包含指定的type

--- create-blogs-index.sh
+++ create-blogs-index.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 #curl -XPOST http://localhost:9200/blogs -d @blogs-index.txt
-curl -XPOST http://localhost:3000/create/blogs/article -d @blogs-index.txt
+curl -XPOST http://localhost:3000/create/blogs -d @blogs-index.txt
 echo
