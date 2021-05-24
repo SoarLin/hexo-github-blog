e78634b03ac2512d524ed535745a1d9c2f45fe50
Soar Lin <soar.lin@gmail.com>
2021-05-24 17:20:48 +0800

    Upgrade gulp 3 to 4

--- gulpfile.js
+++ gulpfile.js
@@ -1,54 +1,88 @@
-var gulp = require('gulp');
+var { series, src, dest } = require('gulp');
 
 // Minify CSS
 var cssnano = require('gulp-cssnano');
 var sourcemaps = require('gulp-sourcemaps');
-gulp.task('minify-css', function () {
-    return gulp.src('./public/css/*.css')
+function minifyCss() {
+    return src('./public/css/*.css')
         .pipe(sourcemaps.init())
         .pipe(cssnano({
             zindex: false
         }))
         .pipe(sourcemaps.write('maps/'))
-        .pipe(gulp.dest('./public/css'));
-});
+        .pipe(dest('./public/css'));
+}
+
+// gulp.task('minify-css', function () {
+//     return gulp.src('./public/css/*.css')
+//         .pipe(sourcemaps.init())
+//         .pipe(cssnano({
+//             zindex: false
+//         }))
+//         .pipe(sourcemaps.write('maps/'))
+//         .pipe(gulp.dest('./public/css'));
+// });
 
 // 壓縮 HTML
 var uglify = require('gulp-uglify');
 var htmlmin = require('gulp-htmlmin');
 var htmlclean = require('gulp-htmlclean');
-gulp.task('minify-html', function() {
-  return gulp.src('./public/**/*.html')
-    .pipe(htmlclean())
-    .pipe(htmlmin({
-         removeComments: true,
-         minifyJS: true,
-         minifyCSS: true,
-         minifyURLs: true,
-    }))
-    .pipe(gulp.dest('./public'))
-});
+function minifyHtml() {
+    return src('./public/**/*.html')
+        .pipe(htmlclean())
+        .pipe(htmlmin({
+            removeComments: true,
+            minifyJS: true,
+            minifyCSS: true,
+            minifyURLs: true,
+        }))
+        .pipe(dest('./public'));
+}
+// gulp.task('minify-html', function() {
+//   return gulp.src('./public/**/*.html')
+//     .pipe(htmlclean())
+//     .pipe(htmlmin({
+//          removeComments: true,
+//          minifyJS: true,
+//          minifyCSS: true,
+//          minifyURLs: true,
+//     }))
+//     .pipe(gulp.dest('./public'))
+// });
 
 // Minify JS
-gulp.task('minify-js', function() {
-    return gulp.src('./public/js/*.js')
+function minifyJs() {
+    return src('./public/js/*.js')
         .pipe(uglify())
-        .pipe(gulp.dest('./public/js'));
-});
+        .pipe(dest('./public/js'));
+}
+// gulp.task('minify-js', function() {
+//     return gulp.src('./public/js/*.js')
+//         .pipe(uglify())
+//         .pipe(gulp.dest('./public/js'));
+// });
 
 
 // 壓縮圖片
 var imagemin = require('gulp-imagemin')
-gulp.task('images', function () {
-    gulp.src('./public/images/**/*.*')
+function images() {
+    return src('./public/images/**/*.*')
         .pipe(imagemin({
             progressive: true
         }))
-        .pipe(gulp.dest('./public/images'))
-});
+        .pipe(dest('./public/images'))
+}
+// gulp.task('images', function () {
+//     gulp.src('./public/images/**/*.*')
+//         .pipe(imagemin({
+//             progressive: true
+//         }))
+//         .pipe(gulp.dest('./public/images'))
+// });
 
 
 // 执行 gulp 命令时执行的任务
-gulp.task('default', [
-    'minify-html','minify-css','minify-js','images'
-]);
+// gulp.task('default', [
+//     'minify-html','minify-css','minify-js','images'
+// ]);
+exports.default = series(minifyHtml, minifyCss, minifyJs, images)
